d4d6c5a2ee1702691287f407c66ff396f1fccfa5
Brandonflex <brandonflex108@gmail.com>
2025-03-25 07:38:07 +0300

    Create Products.js

---
+++ src/Components/Products/Products.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Products.css';
+
+const Products = () => {
+  return (
+    <div className="products">
+      <h1>Products</h1>
+      <p>Here you can find a variety of products available for purchase.</p>
+      {/* Add product listing here */}
+    </div>
+  );
+};
+
+export default Products;

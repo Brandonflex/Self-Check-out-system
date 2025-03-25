00fb9d2e1f3d6e073d5b8dd0a11004d7cd01f562
Brandonflex <brandonflex108@gmail.com>
2025-03-25 07:38:13 +0300

    Create Home.js

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+const Home = () => {
+  return (
+    <div className="home">
+      <h1>Welcome to the Self Checkout System</h1>
+      <p>Start shopping by browsing our products.</p>
+    </div>
+  );
+};
+
+export default Home;
